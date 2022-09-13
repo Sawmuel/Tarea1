1adde2265c6ea97704ba7a18addbdd01ca55bcb7
SAMUEL <SAMUEL@DESKTOP-425T00O.mshome.net>
2022-09-13 11:35:03 -0500

    Tarea01

---
+++ tarea01/src/main/java/upeu/edu/pe/tarea01/Tarea01Application.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.tarea01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea01Application.class, args);
+	}
+
+}
---
+++ tarea01/src/test/java/upeu/edu/pe/tarea01/Tarea01ApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.tarea01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
